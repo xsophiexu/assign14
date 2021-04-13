aa8304d46101fd4bf39feff99a5ebf2b30aa1771
Sophie <xsophiexu@gmail.com>
2021-04-13 13:00:07 -0400

    4.13

---
+++ script.js
@@ -0,0 +1,139 @@
+let outputElement = document.getElementById('outputElement');
+// let outputParagraph = document.getElementById('outputParagraph');
+let contentGridElement = document.getElementById('contentGrid');
+
+let myJSON = {
+  "title" : "Web Exploration",
+  "picture_url" : "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Lil-Bub-2013.jpg/320px-Lil-Bub-2013.jpg",
+  "short" : "ascii art rpg",
+  "back_color" : "#FF0072",
+  "link" : "https://candybox2.github.io/",
+  "activites" : ["single player", "dog", "roomba"]
+}
+
+var randomIndex = Math.floor(Math.random() * myJSON["activites"].length);
+
+let jsonDatabase = [
+  {
+    "title" : "Candy Box",
+    "picture_url" : "candy.jpg",
+    "short" : "ascii art rpg, save slots available offline",
+    "back_color" : "#9FD2D6",
+    "link" : "https://candybox2.github.io/",
+    "tag" : ["single player", "play", "art"]
+  },
+  {
+    "title" : "Tetris",
+    "picture_url" : "tetris.jpg",
+    "short" : "classic online game, can play in public lobbies or make private rooms with up to 99 players",
+    "back_color" : "#8154EF",
+    "link" : "https://jstris.jezevec10.com/",
+    "tag" : ["online", "popular", "single player", "multiplayer"]
+  },
+  {
+    "title" : "Online Go",
+    "picture_url" : "go.png",
+    "short" : "set up private games, play puzzles, compete in tournaments, get rankings and more",
+    "back_color" : "#FF4E4E",
+    "link" : "https://online-go.com/",
+    "tag" : ["1-2 players", "play", "board game"]
+  },
+  {
+    "title" : "Gather Town",
+    "picture_url" : "gather.jfif",
+    "short" : "virtual spaces for video-calling, simulating buildings and rooms in a video game aesthetic",
+    "back_color" : "#c1a411",
+    "link" : "https://gather.town/",
+    "tag" : ["video calling", "interactive"]
+  },
+  {
+    "title" : "Codenames",
+    "picture_url" : "codenames.jpg",
+    "short" : "a clue giving and solving game based on word associations, rec. 2-10 players, but can house more",
+    "back_color" : "#009EB6",
+    "link" : "https://codenames.game/",
+    "tag" : ["card game", "multiplayer", "popular"]
+  },
+  {
+    "title" : "Fishbowl",
+    "picture_url" : "fishbowl.jpg",
+    "short" : "also called Monikers, this is a fun guessing game with Taboo, Charades and Password",
+    "back_color" : "#FF8601",
+    "link" : "https://fishbowl-game.com/",
+    "tag" : ["4+ players", "card game", "mobile avail"]
+  },
+  {
+    "title" : "Reversi",
+    "picture_url" : "reversi.png",
+    "short" : "play this strategy game casually with private game rooms or competitively with live opponents, user profiles and stats, rankings and more",
+    "back_color" : "#405f7c",
+    "link" : "https://www.playok.com/en/reversi/#",
+    "tag" : ["board game", "1-2 players", "play", "mobile avail"]
+  },
+  {
+    "title" : "Type Racer",
+    "picture_url" : "typeracer.jpg",
+    "short" : "race against live opponents and test your speed in an online typing competition",
+    "back_color" : "#ff0072",
+    "link" : "https://play.typeracer.com/",
+    "tag" : ["multiplayer", "play", "popular"]
+  },
+  {
+    "title" : "Online Go",
+    "picture_url" : "go.png",
+    "short" : "set up private games, play puzzles, compete in tournaments, get rankings and more",
+    "back_color" : "#0000ff",
+    "link" : "https://online-go.com/",
+    "tag" : ["1-2 players", "play", "board game"]
+  }
+];
+
+// createElementMessy(myJSON); #ff0072
+
+//createElementProper(jsonDatabase[0]);
+
+for (var i = 0; i < jsonDatabase.length; i++) {
+  createElementProper(jsonDatabase[i]);
+}
+
+function createElementProper(incomingJSON) {
+  /// Create whole content item div and set class
+  let newContentElement = document.createElement("DIV");
+  // newContentElement.style.backgroundColor = incomingJSON['back_color'];
+  newContentElement.classList.add('contentItem');
+
+  let newContentHeading = document.createElement("a");
+  newContentHeading.classList.add('contentTitle');
+  newContentHeading.innerText = incomingJSON['title'];
+  newContentHeading.style.color = incomingJSON['back_color'];
+  if(typeof incomingJSON['link'] !== 'undefined'){
+    newContentHeading.classList.add('activityLink');
+    newContentHeading.setAttribute('href',incomingJSON['link']);}
+  newContentElement.appendChild(newContentHeading);
+
+  /// Create & add footer image
+  let newImage = document.createElement("IMG");
+  newImage.classList.add("jsonImage");
+  newImage.src = incomingJSON['picture_url'];
+  newContentElement.appendChild(newImage);
+
+  /// Create & add LIST HEADLINE to the item
+  let newContentSubhead = document.createElement("H4");
+  newContentSubhead.innerText = incomingJSON['short'];
+  newContentElement.appendChild(newContentSubhead);
+
+  let newContentTagList = document.createElement("DIV");
+  newContentTagList.classList.add('tagList');
+  newContentElement.appendChild(newContentTagList);
+
+  for (var i = 0; i < incomingJSON['tag'].length; i++) {
+    var currentTagString = incomingJSON['tag'][i];
+    var newTagItem = document.createElement("p");
+    newTagItem.innerText = currentTagString;
+    newTagItem.classList.add('tags');
+    newTagItem.style.backgroundColor = incomingJSON['back_color'];
+    newContentTagList.appendChild(newTagItem);
+  }
+
+  contentGridElement.appendChild(newContentElement);
+}
